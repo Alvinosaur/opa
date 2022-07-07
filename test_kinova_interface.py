5e96073c3401d6a8198e4b2fe126ae0f22acaa2d
ruichen-v <richen@umich.edu>
2022-07-07 14:25:11 -0400

    sending cartesian pose to kinova works

--- test_kinova_interface.py
+++ test_kinova_interface.py
@@ -1,8 +1,9 @@
+#!/home/ruic/Documents/meta_cobot_wksp/src/meta_cobot_learning/venv3/bin/python
 import numpy as np
 import time
 
 import rospy
-from hri_tasks_msgs.msg import PoseSync
+# from hri_tasks_msgs.msg import PoseSync
 from geometry_msgs.msg import PoseStamped
 
 from scipy.spatial.transform import Rotation as R
@@ -20,8 +21,9 @@ def robot_pose_cb(msg):
 
 if __name__ == '__main__':
     rospy.init_node('test_kinova_interface')
-    rospy.Subscriber('/robot_cartesian_pose', PoseSync, robot_pose_cb)
-    pose_pub = rospy.Publisher("/kinova_pose_ref", PoseStamped, queue_size=10)
+    rospy.Subscriber('/kinova/pose_tool_in_base_fk',
+                     PoseStamped, robot_pose_cb, queue_size=1)
+    pose_pub = rospy.Publisher("/kinova_demo/pose_cmd", PoseStamped, queue_size=10)
 
     """
     Actual initial pose:
@@ -30,24 +32,24 @@ if __name__ == '__main__':
         float init_z_ = 0.348
     """
 
-    target_pos = np.array([0.406, -0.158, 0.348]) + np.array([0.05, 0.0, 0.0])
+    target_pos = np.array([0.406, -0.158, 0.348]) + np.array([0.1, 0.2, 0.15])
     target_ori = target_pose = None
 
     start_t = time.time()
     while not rospy.is_shutdown():
         if robot_pose is not None:
-            print(robot_pose)
-            print(robot_pose.pose.position)
+            # print(robot_pose)
+            # print(robot_pose.position)
             if target_ori is None:
-                target_ori = R.from_euler('zyx',
-                                          [robot_pose.pose.orientation.z,
-                                           robot_pose.pose.orientation.y,
-                                           robot_pose.pose.orientation.x])
+                target_ori = np.array([robot_pose.orientation.x,
+                                        robot_pose.orientation.y,
+                                        robot_pose.orientation.z,
+                                        robot_pose.orientation.w])
                 target_pose = np.concatenate(
-                    [target_pos, target_ori.as_quat()])
+                    [target_pos, target_ori])
 
-            # if time.time() - start_t > 3.0:
-            #     pose_pub.publish(pose_to_msg(target_pos))
+            if time.time() - start_t > 1.0:
+                pose_pub.publish(pose_to_msg(target_pose))
 
         else:
             print('No robot pose received...')
